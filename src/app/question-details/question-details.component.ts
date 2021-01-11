fed222868fbc9a4851ab4938132bba3eb4a1dd8c
sahil <sahilmortuza.90@gmail.com>
2021-01-11 21:14:44 +0530

    updated basic comment add & reply add feature in question detail page

--- src/app/question-details/question-details.component.ts
+++ src/app/question-details/question-details.component.ts
@@ -12,12 +12,16 @@ export class QuestionDetailsComponent implements OnInit {
 
   loginUser: string;
 
+  newComment: string;
+  replyList: string[];
+
   questionDetail: any;
   ngOnInit() {
     this.loginUser = sessionStorage.getItem('userName');
     this.basicService.getQuestionDetail().subscribe((data: any) => {
       this.questionDetail = data;
       console.log(this.questionDetail);
+      this.replyList = Array(this.questionDetail.comments.length).fill('');
     },
     error => {
       this.basicService.openSnackBar('Error loading threads!');
@@ -26,6 +30,44 @@ export class QuestionDetailsComponent implements OnInit {
 
   get date() {
     return new Date();
+  }
+
+  addComment() {
+    const newComment = {
+      attachment: [],
+      content: this.newComment,
+      createdTime: new Date().toISOString(),
+      creator: {
+        id: sessionStorage.getItem('userId'),
+        label: null,
+        name: sessionStorage.getItem('userName'),
+        photoUrl: null,
+        type: "ENDUSER"
+      },
+      id: Math.floor(Math.random()*500000).toString(),
+      modifiedTime: null,
+      replies: [],
+      status: "PUBLISHED"
+    };
+    this.questionDetail.comments.push(newComment);
+    this.newComment = '';
+  }
+
+  addReply(index: number) {
+    this.questionDetail.comments[index].replies.push({
+      content: this.replyList[index],
+      createdTime: new Date().toISOString(),
+      creator: {
+        id: sessionStorage.getItem('userId'),
+        label: null,
+        name: sessionStorage.getItem('userName'),
+        photoUrl: null,
+        type: "ENDUSER"
+      },
+    });
+    this.questionDetail.comments[index].replies = this.questionDetail.comments[index].replies.slice();
+    this.replyList[index] = '';
+    console.log(this.questionDetail);
   }
 
 }
